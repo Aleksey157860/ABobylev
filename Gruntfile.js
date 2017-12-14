ce097bb32d9ab90e31df095d1fa2337e05d6c5f3
Aleksey157860 <a_lex84@mail.ru>
2017-12-14 15:27:07 +0300

    создал правило для компиляции в папку build. SCSS в CSS пока не работает

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
         scripts: {
             src: 'scripts/*.js',
@@ -12,34 +13,21 @@ module.exports = function(grunt) {
             dest: 'build/sass/main.scss'
         }
     },
+
     sass: {
-        options: {
-            sourceMap: true
-        },
-        dist: {
-            files: {
-                'build/css/main.css': 'build/sass/main.scss'
-            }
-        }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'build/css/main.css': 'sass/**/*.scss',
         }
-      }
     },
+
     qunit: {
-      files: ['test/**/*.html']
+      files: ['*.html']
     },
+
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'scripts/*.js'],
       options: {
-        // options here to override JSHint defaults
-        globals: {
+          globals: {
           jQuery: true,
           console: true,
           module: true,
@@ -47,20 +35,39 @@ module.exports = function(grunt) {
         }
       }
     },
+
     reload: {
         port: 6001,
         proxy: {
             host: 'localhost'
         }
     },
+
     watch: {
       files: ['*.html', 'sass/**/*.scss', 'scripts/*.js'],
       tasks: ['jshint', 'qunit', 'reload']
     },
+
     clean: {
         files: [
             'build/**'
         ]
+    },
+
+    connect: {
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: ['build']
+                }
+            }
+        }
     }
   });
 
@@ -69,12 +76,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-reload');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('build', ['clean', 'jshint', 'concat', 'sass']);
   
 };
